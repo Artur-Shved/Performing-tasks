085e3325f944567e9320e507822fd69656777469
Artur-Shved <69045284+Artur-Shved@users.noreply.github.com>
2020-10-20 22:34:36 +0300

    Create tenRun.java

---
+++ tenRun.java
@@ -0,0 +1,24 @@
+
+/*For each multiple of 10 in the given array, change all the values following it to be that multiple of 10, 
+until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+*/
+
+public int[] tenRun(int[] nums) {
+  int a =1;
+  for(int i=0;i<nums.length;i++){
+  
+    if( nums[i]%10==0){
+  
+       a = nums[i];
+    }
+    
+  
+    if(a==1 ){
+      nums[i] = nums[i];
+    }else{
+    nums[i]=a;
+    }
+  }
+  return nums;
+}
+
